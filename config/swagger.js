48291af4b49d1da0704d480c84e495ed2588a2f2
Aya Sahmi <tayasahmilam@gmail.com>
2025-04-30 10:48:13 +0100

    new updates

--- config/swagger.js
+++ config/swagger.js
@@ -13,7 +13,7 @@ const swag = {
                 url: 'http://localhost:5000',
             },
             {
-                url: 'https://d30e6c47-601f-4639-85d1-4f2aa05d5e11-00-17cyw139my6yf.kirk.replit.dev/docs/'
+                url: 'https://d30e6c47-601f-4639-85d1-4f2aa05d5e11-00-17cyw139my6yf.kirk.replit.dev/'
             },
         ],
         components: {
